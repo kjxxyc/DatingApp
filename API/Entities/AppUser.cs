bb7e0f068107c678ec2d17f0ee6ea53f222ae0a8
Kevin <wasken17@hotmail.com>
2023-05-25 06:08:37 -0600

    END OF SECTION 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        // [Key]
+        public int Id { get; set; }
+        
+        public string UserName { get; set; } //= string.Empty;
+    }
+}
